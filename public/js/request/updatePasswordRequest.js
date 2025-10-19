7a4903df44b4c07c67d9cf1e80baa493bfbb6868
ert1015 <sde101573@gmail.com>
2025-10-19 16:25:48 +0900

    [feat] 비밀번호 변경 요청

---
+++ public/js/request/updatePasswordRequest.js
@@ -0,0 +1,39 @@
+document.getElementById("loginForm").addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("passwordConfirm").value;
+
+    if (password !== passwordConfirm) {
+        alert("비밀번호가 일치하지 않습니다.")
+        return;
+    }
+    const requestBody = {
+        password,
+        passwordConfirm
+    }
+
+    try {
+        const response = await fetch("http://localhost:8080/member/password", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestBody),
+            credentials: "include"
+        });
+
+        if (response.ok) {
+            alert('비밀번호가 성공적으로 변경되었습니다.');
+        } else {
+            const errorData = await response.json();
+            alert(errorData.message);
+            // alert(errorData.message || '비밀번호 변경에 실패했습니다.');
+        }
+
+    }catch (error) {
+        alert(error.response.data);
+    }
+
+
+});
