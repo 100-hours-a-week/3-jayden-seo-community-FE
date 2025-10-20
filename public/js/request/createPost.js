405b3296103eeeaca72c996542de928b2b5d95f3
ert1015 <sde101573@gmail.com>
2025-10-20 21:55:02 +0900

    [feat] 게시글 생성 페이지 요청 구현

---
+++ public/js/request/createPost.js
@@ -0,0 +1,62 @@
+const input = document.getElementById('imageInput');
+const label = document.getElementById('fileLabel');
+
+input.addEventListener('change', () => {
+    if (input.files.length > 0) {
+        label.textContent = input.files[0].name;
+    } else {
+        label.textContent = '파일을 선택해주세요.';
+    }
+});
+
+document.getElementById('postForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value.trim();
+    const content = document.getElementById("content").value.trim();
+    const imageInput = document.getElementById("imageInput");
+    const fileLabel = document.getElementById("fileLabel");
+
+    if(!title || !content) {
+        alert("제목과 내용 모두 입력해주세요.");
+    }
+
+    // ✅ 임시 이미지 경로 (파일명 기반)
+    const imageUrls = ["example1@example.com", "example2@example.com"];
+    // if (imageInput.files.length > 0) {
+    //     // 실제 업로드는 안 하지만, 예시로 임시 경로 구성
+    //     for (const file of imageInput.files) {
+    //         imageUrls.push(`/temp/uploads/${file.name}`);
+    //     }
+    // }
+
+    const postData = {
+        title,
+        content,
+        imageUrls,
+    }
+
+    try{
+        const response = await fetch("http://localhost:8080/post", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
+
+        });
+
+        if(response.ok){
+            alert("게시글이 성공적으로 작성되었습니다.");
+            window.location.assign("/posts.html");
+            // document.getElementById("postForm").reset();
+            // fileLabel.textContent = "파일을 선택해주세요.";
+        }else {
+            const error = await response.json();
+            alert("게시글 등록 실패: " + error.message);
+        }
+    }catch (error) {
+        console.error("Error:", error);
+        alert("요청 중 오류가 발생했습니다.");
+    }
+})
