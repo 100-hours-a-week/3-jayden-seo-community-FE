7459a5e7e35e6b200def0e0ff2c59b4fa50e4d1f
ert1015 <sde101573@gmail.com>
2025-10-22 17:30:55 +0900

    [feat] 이메일 중복 여부 체크 api 연동

--- public/js/api/member/myPage.js
+++ public/js/api/member/myPage.js
@@ -1,29 +1,29 @@
-document.getElementById('checkButton').addEventListener('click', () => {
+document.getElementById('checkButton').addEventListener('click', async () => {
     const nickname = document.getElementById('nickname').value.trim();
     const confirmBtn = document.getElementById('confirmButton')
     if (!nickname) {
         alert("닉네임을 입력해주세요.");
-        // return;
+        return;
     }
-    confirmBtn.style.display =
-        confirmBtn.style.display === 'none' ? 'block' : 'none';
-    // try {
-    //     const res = await fetch(`/user/check-nickname?nickname=${encodeURIComponent(nickname)}`, {
-    //         method: 'GET'
-    //     });
-    //     const result = await res.json();
-    //
-    //     if (result.available) {
-    //         alert("사용 가능한 닉네임입니다!");
-    //         document.getElementById('confirmBtn').style.display = 'inline-block'; // ✅ 수정완료 버튼 표시
-    //     } else {
-    //         alert("이미 존재하는 닉네임입니다.");
-    //         document.getElementById('confirmBtn').style.display = 'none'; // ✅ 다시 숨김
-    //     }
-    // } catch (err) {
-    //     console.error(err);
-    //     alert("닉네임 확인 중 오류가 발생했습니다.");
-    // }
+
+    try {
+        const result = await fetch(
+            `http://localhost:8080/member/nickname/duplicate?nickname=${encodeURIComponent(nickname)}`, {
+            method: 'GET'
+        });
+
+        if (result.ok) {
+            alert("사용 가능한 닉네임입니다!");
+            confirmBtn.style.display = 'block'; // ✅ 수정완료 버튼 표시
+        } else {
+            alert("이미 존재하는 닉네임입니다.");
+            console.log(result);
+            confirmBtn.style.display = 'none'; // ✅ 다시 숨김
+        }
+    } catch (err) {
+        console.error(err);
+        alert("닉네임 확인 중 오류가 발생했습니다.");
+    }
 });
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -48,3 +48,37 @@ window.addEventListener('DOMContentLoaded', async () => {
         window.location.href = "/login.html";
     }
 })
+
+document.getElementById("confirmButton").addEventListener("click", async event => {
+    event.preventDefault();
+
+    const nickname = document.getElementById("nickname").value;
+    const profileImageUrl = "exmple@example.com";
+
+    const requestBody = {
+        nickname,
+        profileImageUrl
+    }
+
+    try {
+        const response = await fetch("http://localhost:8080/member", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestBody),
+            credentials: "include"
+        });
+
+        if (response.ok) {
+            alert('회원정보가 성공적으로 변경되었습니다.');
+            window.location.reload();
+        } else {
+            const errorData = await response.json();
+            alert(errorData.message);
+        }
+
+    }catch (error) {
+        alert(error.response.data);
+    }
+});
