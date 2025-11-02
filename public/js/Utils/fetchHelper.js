328df4a3b400a05575c14caf5f247463e1341fba
ert1015 <sde101573@gmail.com>
2025-11-02 18:23:44 +0900

    [feat] 401 응답에 대한 AT 재발급

--- public/js/Utils/fetchHelper.js
+++ public/js/Utils/fetchHelper.js
@@ -17,7 +17,39 @@ export async function apiRequest(url, method = "GET", body = null){
         options.body = JSON.stringify(body)
     }
 
-    const response = await fetch(url, options);
+    let response = await fetch(url, options);
+
+    if(response.status === 401){
+        try{
+            const error = await response.json();
+            if (error.message?.includes("Access Token expired")) {
+
+                const refreshResponse = await fetch(`${SERVER_URL}/auth/refresh`, {
+                    method: "POST",
+                    credentials: "include",
+                    mode: "cors"
+                });
+
+                if(refreshResponse.ok){
+                    const newData = await refreshResponse.json();
+                    const newAccessToken = newData.accessToken;
+                    sessionStorage.setItem("accessToken", newAccessToken);
+
+                    headers.Authorization = `Bearer ${newAccessToken}`;
+                    options.headers = headers;
+                    response = await fetch(url, options);
+                }else{
+                    alert("로그인이 만료되었습니다. 다시 로그인해주세요.");
+                    sessionStorage.removeItem("accessToken");
+                    window.location.href = "/login.html";
+                    return;
+                }
+            }
+        }catch (error){
+            console.error("401 응답 처리 중 오류:", error);
+            window.location.href = "/login.html";
+        }
+    }
 
     if(response.status === 204 || response.status === 201) {
         return null
