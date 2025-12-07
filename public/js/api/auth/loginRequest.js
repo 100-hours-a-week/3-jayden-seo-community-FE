fc4cb148a2da365c31f8638f3dab33ac9e502afb
ert1015 <sde101573@gmail.com>
2025-12-07 15:45:45 +0900

    [refactor] 로그인 alert 제거

--- public/js/api/auth/loginRequest.js
+++ public/js/api/auth/loginRequest.js
@@ -6,10 +6,18 @@ const passwordDocument = document.getElementById("password");
 document.getElementById("loginButton").addEventListener('click', async () => {
     const email = emailDocument.value.trim();
     const password = passwordDocument.value.trim();
+    const emailError = document.getElementById("emailError");
+    const passwordError = document.getElementById("passwordError");
 
     // 입력값 검증
+    emailError.textContent = "";
+    passwordError.textContent = "";
+
     if (!email || !password) {
-        alert(MESSAGES.LOGIN.INPUT_REQUIRED);
+        emailError.textContent = "이메일은 필수입니다.";
+        return;
+    } else if(!password){
+        passwordError.textContent = "비밀번호는 필수입니다.";
         return;
     }
 
@@ -21,12 +29,11 @@ document.getElementById("loginButton").addEventListener('click', async () => {
 
         sessionStorage.setItem("profileImageUrl", IMAGE_SERVEL_URL2 + data.profileImageUrl);
         sessionStorage.setItem("accessToken", data.accessToken);
-        alert(MESSAGES.LOGIN.SUCCESS);
+        // alert(MESSAGES.LOGIN.SUCCESS);
         window.location.href = "/posts.html";
       
     }catch(err){
         console.error(err);
         alert(`${err.message}`);
     }
-
 })
